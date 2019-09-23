bcc807afac0471646e7ef2b4f8fcd6c4c8be29ee
Franz Gollhammer <franz.gollhammer@gmail.com>
2019-09-23 22:04:38 +0200

    Added user registration

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -33,7 +33,14 @@ namespace Server
             services.AddDefaultIdentity<ApplicationUser>()
                 .AddEntityFrameworkStores<AuthenticationContext>();
 
-            services.Configure<IdentityOptions>(options => )
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
